cfcc1178b43d1f8f3cfc7afd26cb647b60260940
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-02-03 18:25:10 +0200

    edit shell scripts to end processes correctly

--- assignment-1-894931/code/mysimbdp-dataingest/consumer/start_consuming.sh
+++ assignment-1-894931/code/mysimbdp-dataingest/consumer/start_consuming.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for i in $(seq 0 $1)
+# arg 1: number of concurrent kafka consumers
+
+stop() {
+  kill -SIGTERM $(jobs -p)
+  wait
+}
+
+trap stop SIGINT
+
+for i in $(seq 1 $1)
 do
   python3 kafka_consumer.py &
 done
 
-#wait
+wait
