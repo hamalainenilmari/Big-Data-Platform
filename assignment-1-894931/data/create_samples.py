a52bc3ec93a00bd3fa42f438ad531cb585f18922
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-02-05 21:19:07 +0200

    edit sample data creator

--- assignment-1-894931/data/create_samples.py
+++ assignment-1-894931/data/create_samples.py
@@ -1,18 +1,18 @@
 """
-Python script for creating multiple different sample data sets from the original source data 
+Python script for creating multiple different sample data sets from the original large source data file
 for simulating ingesting with multiple writers at the same time 
 """
 import csv
 
 if __name__ == '__main__':
-    with open("taxiTrips.csv", "r", newline="") as f:
+    with open("Taxi_Trips__2024-__20250204.csv", "r", newline="") as f: # use here the source data set
         reader = csv.reader(f)
         header = next(reader)  # header line
 
         fileCount = 1
-        while fileCount < 21:
+        while fileCount < 51: # edit this modify number of sample sets created
             chunk = [header]
-            for _ in range(10000): # edit this to control how many rows per one input data file
+            for _ in range(20000): # edit this to control how many rows per one input data file
                 chunk.append(next(reader))
 
             currentFile = f"sample{fileCount}.csv"
