f2dbe40823b342c07867db4dbcaee24f191b12a0
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-03-09 22:28:32 +0200

    add shell script for concurrent kafka producing

---
+++ assignment-2-894931/code/streamingest/producer/start_producing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# arg 1: number of concurrent producers
+# arg 2: kafka broker ip:port (e.g. localhost:9092)
+
+stop() {
+  kill -SIGTERM $(jobs -p)
+  wait
+}
+
+trap stop SIGINT
+for i in $(seq 1 $1)
+do
+  python3 kafka_producer.py -b $2 -i ../../data/sample$i.csv &
+done
+
+wait
