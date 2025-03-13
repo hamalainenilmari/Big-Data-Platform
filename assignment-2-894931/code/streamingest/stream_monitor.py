385e4be6c7b01bb323e625f04c31f38c77bf4ad9
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-03-13 16:40:37 +0200

    add functionality to listen to real time metrics

--- assignment-2-894931/code/streamingest/stream_monitor.py
+++ assignment-2-894931/code/streamingest/stream_monitor.py
@@ -1,19 +1,79 @@
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, Producer
 import argparse
 import json
 from dotenv import load_dotenv
 import logging
 from datetime import datetime
 
-def generateReport(tenant, statistics, badRows):
+tenant_pipelines = {
+    "chicagotenant": {
+        "minimumIngestionSpeed": 10,     # kB/s
+        "minRowsProcessed": 1,           # min number of rows ingested during execution before a problem
+        "maxDiscardedRowsRelation": 0.001 # % of how many rows of input data can be discarded before a problem
+    },
+    "nytenant": {
+        "minimumIngestionSpeed": 2,
+        "minRowsProcessed": 10,
+        "maxDiscardedRowsRelation": 0.01 # lower value, should not have many
+    }
+}
+
+def kafka_delivery_error(err, msg):
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+
+def handleStatistics(statistics, producer):
+    tenantId = statistics["tenant_id"]
+    time = statistics["timestamp"]
+    rows = statistics["rows_processed"]
+    speed = statistics["rows_per_second"] * 48
+    badRows = statistics["discarded_rows"]
+    if rows > 0:
+        relation = badRows / (badRows + rows) # discarded data amount divided by all consumed data
+    else:
+        relation = 1
+    print(statistics)
+     
+    if relation > tenant_pipelines[tenantId]["maxDiscardedRowsRelation"]:
+        print("Max amount of bad rows in ingestion -> inform manager")
+        msg = {
+            "tenant": tenantId,
+            "warning": "maxDiscardedRowsRelation"
+            }
+        json_data=json.dumps(msg)
+        producer.produce("pipeline_execution_warning", json_data.encode('utf-8'), callback=kafka_delivery_error)
+        producer.flush()
+
+    elif rows < tenant_pipelines[tenantId]["minRowsProcessed"]:
+        print("below min amount of rows processed in ingestion -> inform manager")
+        msg = {
+            "tenant": tenantId,
+            "warning": "minRowsProcessed"
+            }
+        json_data=json.dumps(msg)
+        producer.produce("pipeline_execution_warning", json_data.encode('utf-8'), callback=kafka_delivery_error)
+        producer.flush()
+
+    elif speed < tenant_pipelines[tenantId]["minimumIngestionSpeed"]:
+        print("below min ingestion speed -> inform manager")
+        msg = {
+            "tenant": tenantId,
+            "warning": "minimumIngestionSpeed"
+            }
+        json_data=json.dumps(msg)
+        producer.produce("pipeline_execution_warning", json_data.encode('utf-8'), callback=kafka_delivery_error)
+        producer.flush()
+
+
+def generateReport(tenant, statistics, badRows, producer):
     start = datetime.fromtimestamp(statistics["start_time"]).strftime('%Y-%m-%d %H:%M:%S')
     end = datetime.fromtimestamp(statistics["end_time"]).strftime('%Y-%m-%d %H:%M:%S')
     totalTime = statistics["total_time"]
     rows = statistics["rows"]
     size = statistics["total_size"]
-    speed = statistics["speed"]
+    speed = statistics["rows_per_second"] * 48
     print(statistics)
-     
+
     logFile = f"../../logs/stream/{tenant}_{start}_stream_ingestion.log"
     logger = logging.getLogger(f"{tenant}_{start}")
     logger.setLevel(logging.INFO)
@@ -37,7 +97,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--broker', default="localhost:9092", help='Broker as "server:port"')
     parser.add_argument('-t', '--topics', default=["chicagotenant_ingestion_report", "chicagotenant_ingestion_report_warning", \
-                                                   "nytenant_ingestion_report", "nytenant_ingestion_report_warning"], help='kafka topics')
+                                                   "chicagotenant_ingestion_status","nytenant_ingestion_report", \
+                                                    "nytenant_ingestion_report_warning", "ingestion_status"], help='kafka topics')
     parser.add_argument('-g', '--consumer_group', default="monitor", help='consumer group')
     args = parser.parse_args()
     broker=args.broker
@@ -52,6 +113,11 @@ if __name__ == '__main__':
     
     kafka_consumer = Consumer(kafka_conf)
     kafka_consumer.subscribe(topics)
+
+    kafka_conf_p={
+        'bootstrap.servers': broker,
+    }
+    producer = Producer(kafka_conf_p)
 
     tenants = {}
     for item in topics:
@@ -73,12 +139,13 @@ if __name__ == '__main__':
                 split = topic.split("_", 1)
                 tenant = split[0]
                 report = split[1]
-                print("tenant: ", tenant)
-                print("report: ", report)
-                # discarded row information
-                if report == "ingestion_report_warning":
-                    rowsDiscarded = tenants[tenant][1]
-                    tenants[tenant] = (False, rowsDiscarded + 1)
+                print(msg.value())
+                #print("tenant: ", tenant)
+                #print("report: ", report)
+                value = json.loads(msg.value().decode('utf-8'))
+                if report == "ingestion_status":
+                    print(f"Reveived ingestion status from tenant {value['tenant_id']} pipeline:")
+                    handleStatistics(value, producer)
 
                 # final execution statistics
                 if report == "ingestion_report":
@@ -86,7 +153,7 @@ if __name__ == '__main__':
                     stats = json.loads(msg.value().decode('utf-8'))
                     rows = tenants[tenant][1]
                     tenants[tenant] = (False, 0)
-                    generateReport(tenant, stats, rows)
+                    generateReport(tenant, stats, rows, producer)
 
     except KeyboardInterrupt:
         print("Exiting...")
