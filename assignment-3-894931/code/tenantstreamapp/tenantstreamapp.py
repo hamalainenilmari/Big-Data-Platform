7fc973d69a21df94bb69553414b3f0997e46035d
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-03-27 20:48:33 +0200

    add initial metrics

--- assignment-3-894931/code/tenantstreamapp/tenantstreamapp.py
+++ assignment-3-894931/code/tenantstreamapp/tenantstreamapp.py
@@ -1,113 +1,76 @@
-from pyflink.datastream import StreamExecutionEnvironment, TimeCharacteristic
+from pyflink.datastream import StreamExecutionEnvironment, TimeCharacteristic, OutputTag
 from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer, KafkaSink, KafkaRecordSerializationSchema
-from pyflink.datastream.connectors.cassandra import CassandraSink
 from pyflink.common import Types, Row
-from pyflink.common.watermark_strategy import WatermarkStrategy
-from pyflink.common.serialization import SimpleStringSchema, Encoder
 from pyflink.datastream.functions import RuntimeContext
+from pyflink.common.watermark_strategy import WatermarkStrategy
 from pyflink.datastream.state import ValueStateDescriptor
-from pyflink.datastream import OutputTag, DataStream
-from pyflink.datastream import MapFunction
-import math
+from pyflink.common.serialization import SimpleStringSchema, Encoder
 import json
 from datetime import datetime
-import time
 import os
 from confluent_kafka import Consumer, Producer
 from dotenv import load_dotenv
-import threading
-from cassandra.cluster import Cluster
-from pyflink.datastream.functions import KeyedProcessFunction
 from pyflink.datastream.window import TumblingEventTimeWindows
 from pyflink.datastream.functions import ProcessWindowFunction
-from pyflink.common.watermark_strategy import WatermarkStrategy, Duration, TimestampAssigner
+from pyflink.common.watermark_strategy import WatermarkStrategy, TimestampAssigner
 from pyflink.common.typeinfo import Types
 from pyflink.common.time import Time
-from pyflink.datastream.functions import AggregateFunction, ReduceFunction, WindowFunction
-from pyflink.datastream.window import TimeWindow
-from typing import Iterable
 import pytz
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.table import StreamTableEnvironment, EnvironmentSettings
-from pyflink.table.types import DataTypes
-from pyflink.datastream import StreamExecutionEnvironment, CheckpointingMode, RocksDBStateBackend, StateBackend, FsStateBackend
-#from pyflink.datastream.connectors import StreamingFileSink
+from pyflink.datastream import StreamExecutionEnvironment, CheckpointingMode, FsStateBackend
 from pyflink.common.serialization import Encoder
 from pyflink.datastream.connectors.file_system import StreamingFileSink, OutputFileConfig, RollingPolicy
-from pyflink.datastream.connectors import BucketAssigner
-from pyflink.table.descriptors import Schema
-
-def transform(stream):
-     jsonStream = json.loads(stream) # load the source string into json format
-     for key,value in jsonStream.items():
-         if isinstance(value, float) and math.isnan(value): # value is NaN, modify it to null
-             jsonStream[key] = None
- 
-     # format timestamps
-     timestampStart = datetime.strptime(jsonStream["Trip Start Timestamp"], '%Y-%m-%dT%H:%M:%SZ')
-     timestampEnd = datetime.strptime(jsonStream["Trip End Timestamp"], '%Y-%m-%dT%H:%M:%SZ')
- 
-     # Create Flink Row from source, format ready for Cassandra insert
-     # removed Pickup Census Tract, Dropoff Census Tract, Pickup Centroid Location, Dropoff Centroid Location
-     filteredRow = Row(
-             jsonStream["Pickup Community Area"],  
-             jsonStream["Trip ID"],
-             jsonStream["Company"],
-             jsonStream["Dropoff Centroid Latitude"],
-             jsonStream["Dropoff Centroid Longitude"],
-             jsonStream["Dropoff Community Area"],
-             jsonStream["Extras"],
-             jsonStream["Fare"],
-             jsonStream["Payment Type"],
-             jsonStream["Pickup Centroid Latitude"],
-             jsonStream["Pickup Centroid Longitude"],
-             jsonStream["Taxi ID"],
-             jsonStream["Tips"],
-             jsonStream["Tolls"],
-             jsonStream["Trip End Timestamp"],
-             #timestampEnd,
-             jsonStream["Trip Miles"],
-             jsonStream["Trip Seconds"],
-             jsonStream["Trip Start Timestamp"],
-             #timestampStart,
-             jsonStream["Trip Total"]
-             )
-     return filteredRow
- 
-def checkPrimaryKeys(stream):
-     global incorrectRowsAmount
-     try:
-         jsonStream = json.loads(stream) # load the source string into json format
-         if isinstance(jsonStream["Pickup Community Area"], float) and math.isnan(jsonStream["Pickup Community Area"]): 
-             # Primary key is NaN, discard input
-             return False
-         elif isinstance(jsonStream["Trip ID"], float) and math.isnan(jsonStream["Trip ID"]):
-             return False
-         else:
-             # valid data
-             return True
-     except json.JSONDecodeError as e:
-         print(f"Json decoding error: {e}")
-
+import math
+import logging
 
 class TaxiAnalyticsProcessWindowFunction(ProcessWindowFunction):
-    def process(self, key, context, elements):
+    def __init__(self):
+        self.row_count_state = None
+        self.row_count_side_output_tag = OutputTag("row_count_tag", Types.LONG())
 
+    def open(self, runtime_context: RuntimeContext):
+        # initialize counts
+        count_descriptor = ValueStateDescriptor("row_count", Types.LONG())
+        self.row_count_state = runtime_context.get_state(count_descriptor)
+
+    def process(self, key, context, elements):
         # all records of this key
         elements_list = list(elements)
 
-        if not elements_list:
+        #row_count_side_output_tag = OutputTag("row_count_tag", Types.LONG())
+
+        current_count = self.row_count_state.value() or 0
+        row_count = current_count + len(elements_list)
+        self.row_count_state.update(row_count)
+
+        #print("row count: ", self.row_count_state.value())
+        yield self.row_count_side_output_tag, row_count
+        #context.side_output()
+        
+        if not elements_list or len(elements_list) == 0:
+            print("empty list")
             return
         
+        if key is None or (isinstance(key, float) and math.isnan(key)) or key == 'NaN' or key == 'nan':
+            print("error with key")
+            return
+
         # window information
         window = context.window()
+        #context.output(row_count_side_output_tag, row_count)
         
         # Aggregate total number of trips and price
         trip_count = len(elements_list)
-        total_price = sum(elem[1] for elem in elements_list)
+        total_price = sum(elem[2] for elem in elements_list)
+
+        if total_price is None or total_price == 'NaN':
+            print("error with total price")
+            total_price = 0
 
         result = Row(key,trip_count,total_price,window.start,window.end)
         yield result
+
+        #yield row_count_side_output_tag, "something"
 
 
 def parse_datetime(iso_string):
@@ -154,7 +117,7 @@ def cleanse_analytics(element):
             # no need to skip value
             total_price = 0.0
         
-        result = (community_area, total_price)
+        result = (community_area, timestamp, total_price)
         return result
     except Exception as e:
         print(f"Error while cleansing element: {element}, Error: {e}")
@@ -168,6 +131,16 @@ def execute():
 
     env.enable_checkpointing(20000, CheckpointingMode.EXACTLY_ONCE)
     env.set_state_backend(FsStateBackend("hdfs://localhost:9000/flink/checkpoints"))
+
+    logFile = f"../../logs/_stream_analytics.log"
+    logger = logging.getLogger(f"chicago")
+    logger.setLevel(logging.INFO)
+    
+    fileHandler = logging.FileHandler(logFile)
+    fileHandler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(fileHandler)
+   
+    logger.info(f"Starting stream analytics ...")
 
     # JARs of kafka (source) and Cassandra (sink) connectors
     kafka_jar = os.getenv("KAFKA_JAR")
@@ -214,10 +187,8 @@ def execute():
     
     # tenant input streaming data from kafka
     stream = env.from_source(source, WatermarkStrategy.no_watermarks(), "Kafka Source")
-
     # cleanse input data; skip if missing key values
     stream = stream.map(cleanse_analytics)
-
     # assign timestamp for windowing
     stream = stream.assign_timestamps_and_watermarks(
             WatermarkStrategy.for_monotonous_timestamps()
@@ -229,7 +200,7 @@ def execute():
     windowed_stream = (stream
         .filter(lambda x: x is not None)                        # Filter out None values
         .key_by(lambda x: x[0])                                 # Key by community area
-        .window(TumblingEventTimeWindows.of(Time.seconds(20)))  # tumbling window TODO check this
+        .window(TumblingEventTimeWindows.of(Time.seconds(60)))  # tumbling window TODO check this
         .process(                                               # Aggregate number of trips per area & total fares
             TaxiAnalyticsProcessWindowFunction(),
             output_type=Types.ROW([
@@ -240,100 +211,23 @@ def execute():
                 Types.LONG()                                    # window end timestamp
                 ]))
     )
+    row_count_side_output_tag = OutputTag("row_count_tag", Types.LONG())
 
-    windowed_stream.print()
+    row_count_side_stream = windowed_stream.get_side_output(row_count_side_output_tag)
+
+    #row_count_side_stream.print()
+    summed = row_count_side_stream.key_by(lambda x: 1).sum()
+    summed.print()
+    #windowed_stream.print()
 
     # Add the sink to the stream
     csv_stream = windowed_stream.map(lambda row: f"{row[0]},{row[1]},{row[2]},{row[3]},{row[4]}", Types.STRING())
     csv_stream.add_sink(hdfs_sink)
-
-    """
-    stream = stream.map(lambda x: to_tuple(x))
-    stream.print()
+    
 
 
-    watermark_strategy = WatermarkStrategy.for_monotonous_timestamps() \
-        .with_timestamp_assigner(MyTimestampAssigner())
-    #for_bounded_out_of_orderness(Duration.of_seconds(10)) \
-    
-    stream = stream.assign_timestamps_and_watermarks(watermark_strategy)
-
-    windowed_stream = (
-        stream
-        .key_by(lambda x: x[0])  # Key by Trip ID
-        .window(TumblingEventTimeWindows.of(Time.seconds(20)))  # Apply 60s event time window
-        .reduce(SumReduceFunction())  # Reduce by summing values
-    )
-    windowed_stream.print()  
-    """
-    #stream.print()
-
-    #stream.print()
-
-    #filteredStream = stream.filter(checkPrimaryKeys)
-    #filteredStream.print()
-    """
-    processedStream = filteredStream.map(
-         lambda raw: transform(raw),
-         output_type=Types.ROW([
-            Types.FLOAT(),          # pickup_community_area
-            Types.STRING(),         # trip_id
-            Types.STRING(),         # company
-            Types.DOUBLE(),         # dropoff_centroid_latitude
-            Types.DOUBLE(),         # dropoff_centroid_longitude
-            Types.FLOAT(),          # dropoff_community_area
-            Types.FLOAT(),          # extras
-            Types.FLOAT(),          # fare
-            Types.STRING(),         # payment_type
-            Types.DOUBLE(),         # pickup_centroid_latitude
-            Types.DOUBLE(),         # pickup_centroid_longitude
-            Types.STRING(),         # taxi_id
-            Types.FLOAT(),          # tips
-            Types.FLOAT(),          # tolls
-            Types.LONG(),  # trip_end_timestamp
-            Types.FLOAT(),          # trip_miles
-            Types.INT(),            # trip_seconds
-            Types.LONG(),  # trip_start_timestamp
-            Types.FLOAT()           # trip_total
-         ]))
-    
-    processedStream.print()
-    """
-    
-    
-    
-    """
-    filteredStream = stream.filter(checkPrimaryKeys)
-    
-    processedStream = filteredStream.map(
-         lambda raw: transform(raw),
-         output_type=Types.ROW([
-            Types.FLOAT(),          # pickup_community_area
-            Types.STRING(),         # trip_id
-            Types.STRING(),         # company
-            Types.DOUBLE(),         # dropoff_centroid_latitude
-            Types.DOUBLE(),         # dropoff_centroid_longitude
-            Types.FLOAT(),          # dropoff_community_area
-            Types.FLOAT(),          # extras
-            Types.FLOAT(),          # fare
-            Types.STRING(),         # payment_type
-            Types.DOUBLE(),         # pickup_centroid_latitude
-            Types.DOUBLE(),         # pickup_centroid_longitude
-            Types.STRING(),         # taxi_id
-            Types.FLOAT(),          # tips
-            Types.FLOAT(),          # tolls
-            Types.SQL_TIMESTAMP(),  # trip_end_timestamp
-            Types.FLOAT(),          # trip_miles
-            Types.INT(),            # trip_seconds
-            Types.SQL_TIMESTAMP(),  # trip_start_timestamp
-            Types.FLOAT()           # trip_total
-         ]))
-    
-    #processedStream.print()
-    
-   """
     try:
-        res = env.execute("Kafka Flink Cassandra pipeline")
+        res = env.execute("FlinkSilverDataAnalyticsPipeline")
         current_job = res
         job_id = res.get_job_id()
         print("Flink job id: ", job_id)
