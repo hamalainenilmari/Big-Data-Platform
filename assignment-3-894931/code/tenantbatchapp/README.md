2bfd5ddd572e861501b81ee364c332fc8bc615dd
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2025-03-31 11:58:10 +0300

    add info to tenantbatchapp

---
+++ assignment-3-894931/code/tenantbatchapp/README.md
@@ -0,0 +1,3 @@
+# Batch analytics component
+
+Tenantbatchapp.py is the implementation of batch analytics component.
--- assignment-3-894931/code/tenantbatchapp/tenantbatchapp.py
+++ assignment-3-894931/code/tenantbatchapp/tenantbatchapp.py
@@ -2,21 +2,17 @@
 import argparse
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import col
 from dotenv import load_dotenv
-from pyspark.sql.functions import udf, from_unixtime, trim
-from pyspark.sql.types import StringType
 from pyspark.sql import functions as F
 
 def execute():
         load_dotenv()
         parser = argparse.ArgumentParser()
         parser.add_argument("--input_files", nargs="+", help="input data files")
-        #parser.add_argument("--hour", help="hour of data")
 
+        # Get input silver data storage file location from arguments
         args = parser.parse_args()
         files = args.input_files
-        #print(files)
         splitted_name = files[0].split("silverData")
         gold_location = splitted_name[0] + "goldData" + "/" + splitted_name[1].split("/")[1]
         
@@ -24,9 +20,13 @@ def execute():
         spark = SparkSession.builder.appName("BatchAnalytics")\
                 .getOrCreate()
 
+        # Read all silver data data
         df = spark.read.csv(files, header=False, inferSchema=True)
+
+        # Add gold data headers
         df = df.toDF("pickup_location_area", "trips", "total_fares", "window_start_ts", "window_end_ts")
         
+        # Create aggregations
         aggregated_df = df.groupBy("pickup_location_area").agg(
                 F.sum("trips").alias("trips"),
                 F.sum("total_fares").alias("sum_fares"),
@@ -36,7 +36,8 @@ def execute():
         )
         
         aggregated_df.show()
-        
+
+        # Store the result gold data to HDFS gold data storage
         aggregated_df.write \
         .mode("append") \
         .option("header", "true") \
